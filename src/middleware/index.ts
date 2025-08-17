7762d983b50e98d78e9989c96d6ecabaa32a7caa
Lateef Onatunde <olowoibule@gmail.com>
2025-08-17 02:35:04 +0100

    feat: add health check route and error handler

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,3 +3,5 @@ import { Request, Response, NextFunction } from 'express';
 export default function (_req: Request, _res: Response, next: NextFunction): void {
   next();
 }
+
+export { default as errorHandler } from './errorHandler';
